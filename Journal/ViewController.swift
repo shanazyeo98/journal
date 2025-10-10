9468e241396d7dab939fce1a10fdffb0c28183ff
Shanaz <shanazyeo98@gmail.com>
2025-10-10 13:42:00 +0800

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Shanaz Yeo on 10/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
